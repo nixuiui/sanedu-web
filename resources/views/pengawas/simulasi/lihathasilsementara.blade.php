6034e3c1bf96c1141f5e0b5162bec7be374075bf
Niki Rahmadi Wiharto <nikirahmadiwiharto@gmail.com>
2018-10-21 10:32:08 +0700

    change field in table hasil sementara

--- resources/views/pengawas/simulasi/lihathasilsementara.blade.php
+++ resources/views/pengawas/simulasi/lihathasilsementara.blade.php
@@ -7,17 +7,19 @@ Hasil Sementara - {{ $simulasi->judul }}
 @section('content')
 <a href="{{ route('pengawas.simulasi.kelola', $simulasi->id) }}" class="btn btn-default btn-space btn-icon"><i class="mdi mdi-arrow-back"></i>Kembali</a>
 <div class="row">
-    <div class="col-md-12">
+    <div class="col-md-8">
         <div class="panel panel-default panel-table">
             <div class="panel-body table-responsive">
                 <table class="table table-striped">
                     <thead>
                         <tr>
-                            <th width="200px">No Peserta</th>
-                            <th width="300px">Nama</th>
-                            @for($i=0; $i < $jumlahSoal; $i++)
-                            <th class="text-center">{{ $i+1 }}</th>
-                            @endfor
+                            <th>No Peserta</th>
+                            <th>Nama</th>
+                            <th>Asal Sekolah</th>
+                            <th class="text-center">Benar</th>
+                            <th class="text-center">Salah</th>
+                            <th class="text-center">Tidak Diisi</th>
+                            <th class="text-center">Nilai Sementara</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -25,11 +27,11 @@ Hasil Sementara - {{ $simulasi->judul }}
                         <tr>
                             <td>{{ $data->no_peserta }}</td>
                             <td>{{ $data->profil->nama }}</td>
-                            @for($i=0; $i < $jumlahSoal; $i++)
-                            <td class="text-center">
-                                {{ $data->koreksi[$i]->jawaban != null ? $data->koreksi[$i]->jawaban : "-" }}
-                            </td>
-                            @endfor
+                            <td>{{ $data->profil->asal_sekolah }}</td>
+                            <td class="text-center">{{ $data->jumlah_benar }}</td>
+                            <td class="text-center">{{ $data->jumlah_salah }}</td>
+                            <td class="text-center">{{ $data->jumlah_tidak_jawab }}</td>
+                            <td class="text-center">{{ $data->jumlah_benar }}</td>
                         </tr>
                         @endforeach
                     </tbody>
