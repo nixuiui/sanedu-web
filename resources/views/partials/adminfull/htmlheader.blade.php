0780f847f6d5d02624bbd9ab206befbecdf2b70b
Niki Rahmadi Wiharto <nikirahmadiwiharto@gmail.com>
2018-10-24 00:30:09 +0700

    add google webmaster and analytics

--- resources/views/partials/adminfull/htmlheader.blade.php
+++ resources/views/partials/adminfull/htmlheader.blade.php
@@ -4,6 +4,7 @@
 <!-- @nikirahmadi -->
 <!-- START HEAD -->
 <head>
+    @include('partials.include.google')
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
---
+++ resources/views/partials/include/google.blade.php
@@ -0,0 +1,10 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-56070988-8"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-56070988-8');
+</script>
+<meta name="google-site-verification" content="gdY1fWFw6mEK5PxMjRz1tnja8eFA1299ZsBEk6XmSWo" />
