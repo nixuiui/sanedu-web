11083d68e9b4ed096dc030bd96ebb5351bc6ab55
root <root@vultr.guest>
2018-11-06 12:40:07 +0000

    -

--- routes/web.php
+++ routes/web.php
@@ -221,6 +221,7 @@ Route::group(['middleware' => 'adminsimulasi', 'prefix' => 'adminsimulasi'], fun
             Route::post('/deletejawaban',     'AdminSimulasi\SimulasiController@deleteJawaban')->name('adminsimulasi.simulasi.kelola.delete.jawaban');
             Route::get('/pindahruang',                  'AdminSimulasi\SimulasiController@pindahRuang')->name('adminsimulasi.simulasi.kelola.pindah.ruang');
             Route::get('/koreksi',                      'AdminSimulasi\SimulasiController@koreksi')->name('adminsimulasi.simulasi.kelola.koreksi');
+            Route::get('/generateattempt',              'AdminSimulasi\SimulasiController@generateAttempt');
             Route::post('/koreksi',                     'AdminSimulasi\SimulasiController@koreksiPost')->name('adminsimulasi.simulasi.kelola.koreksi.post');
         });
     });
